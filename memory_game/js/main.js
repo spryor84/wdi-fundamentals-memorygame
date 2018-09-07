87497dc21e01f7043286efa3c5f1b235d2ec1a00
Sam Pryor <sampryor@SBPs-MacBook-Air.local>
2018-09-07 12:24:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,18 +1,22 @@
-console.log("Up and running!");
 var cards = ["queen","queen","king","king"];
 var cardsInPlay = []
-var cardOne
-var cardOne = cards[0]
-cardsInPlay.push(cardOne);
-console.log("User flipped queen");
-var cardTwo
-var cardTwo = cards[2]
-cardsInPlay.push(cardTwo);
-console.log("User flipped king");
-if (cardsInPlay.length === 2) {
+
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+	alert("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+	alert("Sorry, try again.");
 	}
-}	
+}
+
+var flipCard = function(cardId) {
+console.log("User flipped " + cards[cardId]);
+cardsInPlay.push(cards[cardId]);
+checkForMatch();
+};
+
+flipCard(0);
+flipCard(2);
+
+// define cardId?
+// On the line after the flipCard function, call the flipCard function, passing in 0 as an argument.
